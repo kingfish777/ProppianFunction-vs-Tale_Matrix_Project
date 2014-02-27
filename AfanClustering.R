33f1d7ff13645abf3b3e560b7a255d314fce3856
Kingfish Threesevens <scott.malec@gmail.com>
2014-02-27 17:24:56 -0500

    Update AfanClustering.R

--- AfanClustering.R
+++ AfanClustering.R
@@ -4,27 +4,26 @@ library(ape)
 library(RTextTools)
 library(Rgraphviz)
 home <- "/home/hinckley"
-homePath = paste(home, "/Public/DFM", sep="")
+homePath = paste(home, "/Public/DFuncM", sep="")
 setwd(paste(homePath, sep=""))
 text <- system.file("texts", "txt", package="tm");
 corpus <- Corpus(DirSource())
-
-corpus <- tm_map(corpus, removePunctuation)
-corpus <- tm_map(corpus, removeNumbers)
-corpus <- tm_map(corpus, tolower)
-corpus <- tm_map(corpus, removeWords, c("MOVE"))
-corpus <- tm_map(corpus, stripWhitespace)
-corpus <- tm_map(corpus, stemDocument, language = "english")
-#ngrams <- RWeka::NGramTokenizer(corpus, Weka_control(min=1, max=4))
-dtm <- DocumentTermMatrix(corpus, control = list(weighting = weightTfIdf))
+#corpus <- tm_map(corpus, removePunctuation)
+#corpus <- tm_map(corpus, removeNumbers)
+#corpus <- tm_map(corpus, toupper)
+#corpus <- tm_map(corpus, removeWords, c("MOVE")) #remove other functions
+#corpus <- tm_map(corpus, stripWhitespace)
+ngrams <- RWeka::NGramTokenizer(corpus, Weka_control(min=1, max=3))
+dtm <- DocumentTermMatrix(corpus, control = list(ngrams, weighting = weightTfIdf))
 #dtm <- create_matrix(cbind(as.vector(corpus)), language="english", minDocFreq=1, maxDocFreq=Inf,
-#              minWordLength=3, maxWordLength=Inf, ngramLength=3, originalMatrix=NULL,
-#              removeNumbers=FALSE, removePunctuation=TRUE, removeSparseTerms=0,
-#              removeStopwords=TRUE, stemWords=FALSE, stripWhitespace=TRUE, toLower=TRUE,
-#              weighting=weightTf)
-rowTotals <- apply(dtm , 1, sum) #Find the sum of words in each Document
-dtm <- dtm[rowTotals> 0] #remove all docs without words
-dtm <- removeSparseTerms(dtm, .95)
+# minWordLength=3, maxWordLength=Inf, ngramLength=3, originalMatrix=NULL,
+# removeNumbers=FALSE, removePunctuation=TRUE, removeSparseTerms=0,
+# removeStopwords=TRUE, stemWords=FALSE, stripWhitespace=TRUE, toLower=TRUE,
+# weighting=weightTf)
+#rowTotals <- apply(dtm , 1, sum) #Find the sum of words in each Document
+#dtm <- dtm[rowTotals> 0] #remove all docs without words
+#dtm <- removeSparseTerms(dtm, .99)
+dtm_complete <- hclust(dist(dtm), members=NULL, method="centroid")
 plot(hclust(dist(dtm), method="complete"), xlab="text from corpus", "ylab"="distance", main="Cluster Dendrogram \n of Various Russian Magic Tales")
 op = par(bg="#DDE3CA")
 plot(dtm_complete, col="#487AA1", col.main="#45ADA8", col.lab="#7C8071",
@@ -36,5 +35,7 @@ nplot(dtm_complete, hang=1, axes = TRUE, ann=TRUE, main = "Cluster Dendrogram Re
       xlab="Magic Tale Name", ylab = "Distance")
 phyl <- as.phylo(hclust(dtm_distro))
 plot(phyl, edge.col=c("blue", "green", "red")[c(TRUE, FALSE) + 1 + (phyl$edge.length > 20)])
+
+
 
 
